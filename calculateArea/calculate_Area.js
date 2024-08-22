2bb64224b46bb674968d989859871b1f72989556
Deme2125 <dadugnaw@gmail.com>
2024-08-22 03:22:25 -0400

    added

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,5 +1,9 @@
 let length;
 let width;
+let amount1;
+let amount2;
+let amount3;
+
 
 function calculateArea ()
 {
@@ -10,4 +14,13 @@ function calculateArea ()
     // document.getElementById('result').innerText='the area is', area;
      //document.getElementById('result').innerText='The area of the rectangle is: ${area}';
      document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
+
+function groceryTracker ()
+{
+amount1=parseFloat(document.getElementById('grocery1').value);
+amount2=parseFloat(document.getElementById('grocery2').value);
+amount3=parseFloat(document.getElementById('grocery3').value);
+let rr = amount1 * amount2 *amount3;
+document.getElementById('rr').innerText = `The Total amount is :$ ${rr}`;
 }
